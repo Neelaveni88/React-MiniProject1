f79a37ef05ce81dec0bb1130189ff5f9ba037304
Deepa-Senthil <deepasenthil5812@gmail.com>
2025-03-18 15:27:42 +0530

    remove the product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom"; // Ensure you import from react-router-dom
 
 import Layout from "./layout/Layout";
-import Product from "./product/Product";
 import HomePage from "./Home/HomePage";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/product" element={<Product />} />
       </Route>
     </Routes>
   );
